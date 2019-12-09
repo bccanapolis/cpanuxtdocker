5084fbbe57267f8fb2f86c6a0e8f1aa9c700b2bb
Bruno de Araujo Alves devbaraus <devbaraus@gmail.com>
2019-12-09 08:52:49 -0300

    mudando págica inicial

---
+++ layouts/error.vue
@@ -0,0 +1,25 @@
+<template>
+  <card>
+    <template v-slot:content>
+      <div style="height: 25vh;"></div>
+      <h1 class="text-center" v-if="error.statusCode === 404">Página não encontrada</h1>
+      <h1 class="text-center" v-else>Aconteceu algum problema.</h1>
+      <div style="height: 25vh;"></div>
+      <h4>
+        Volte para a página inicial: <nuxt-link tag="a" to="/">Home</nuxt-link>
+      </h4>
+    </template>
+  </card>
+
+</template>
+
+<script>
+    import card from "../components/card";
+
+    export default {
+        props: ['error'],
+        components: {
+            card
+        }
+    }
+</script>
