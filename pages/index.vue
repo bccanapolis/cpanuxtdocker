5f2d7f27f4e016515df13a1de172ddfa82e82928
Bruno de Araujo Alves  devbaraus <contato@baraus.dev>
2020-10-16 20:11:25 -0300

    Adicionando tela de respostas e link pra gráficos

--- pages/index.vue
+++ pages/index.vue
@@ -3,8 +3,15 @@
     <card>
       <template v-slot:content>
         <div style="height: 20vh;"></div>
-        <h2 class="text-center">Relátorio será divulgado em breve.</h2>
-        <h3 class="text-center">Dados em análise.</h3>
+        <h2 class="text-center">Relatórios de Autoavaliação 2019</h2>
+        <p class="text-justify px-5">O Relatório de Autoavaliação Institucional do IFG visa fomentar a cultura de
+          avaliação institucional e é compreendido de forma substancial e não só como mero procedimento burocrático ou
+          movimento de avaliar por avaliar. Busca contribuir efetivamente para promoção de melhorias na Instituição por
+          meio da disponibilização dos dados, análises e sugestões da CPA.</p>
+        <div style="display: flex; justify-content: center;">
+          <a download="true" href="/relatorio_cpa_ifg_2019.pdf" class="btn btn-success text-center text-uppercase">baixar</a>
+        </div>
+
         <div style="height: 20vh;"></div>
       </template>
     </card>
@@ -12,14 +19,23 @@
 </template>
 
 <script>
-    import card from "../components/card";
+import card from "../components/card";
 
-    export default {
-        name: "index",
-        components: {card}
-    }
+export default {
+  name: "index",
+  components: {card}
+}
 </script>
 
 <style scoped>
+.px-5 {
+  padding: 0;
+}
+
+@media (min-width: 960px) {
+  .px-5 {
+    padding: 0 5rem;
+  }
+}
 
 </style>
