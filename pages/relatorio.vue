ce2a5f8dc77854d60a3caa0d4c76251e13e14e1a
Bruno de Araujo Alves  devbaraus <contato@baraus.dev>
2020-10-16 20:31:45 -0300

    Adicionando tela de respostas e link pra gráficos

--- pages/relatorio.vue
+++ pages/relatorio.vue
@@ -9,16 +9,14 @@
           <div v-if="queryChart.pergunta != 0 && indicatorChart" class="chart-options">
             <p class="lead small text-uppercase">Indicador</p>
             <h3 id="chart-indicator" class="lead" :style="`background-color:${indicatorChart.cor}`">
-              {{indicatorChart.label}} - {{indicatorChart.valor}}%</h3>
+              {{ indicatorChart.label }} - {{ indicatorChart.valor }}%</h3>
             <hr>
           </div>
 
           <chart :query="queryChart" :normalize="normalChart" :totalize="totalChart" @fetch="chartData"></chart>
           <hr>
-          <div class="row">
-            <div class="col-sm-12">
-              <tablechart :fields="queryChart" :pie="queryChart.pergunta == 0" :query="tableChart"></tablechart>
-            </div>
+          <div class="row" style="overflow-x: auto;">
+            <tablechart :fields="queryChart" :pie="queryChart.pergunta == 0" :query="tableChart"></tablechart>
           </div>
 
         </div>
@@ -34,49 +32,49 @@
 </template>
 
 <script>
-    import card from "../components/card";
-    import filterchart from "../components/filterchart";
-    import chart from "../components/chart/chart";
-    import tablechart from "../components/tablechart/tablechart";
+import card from "../components/card";
+import filterchart from "../components/filterchart";
+import chart from "../components/chart/chart";
+import tablechart from "../components/tablechart/tablechart";
 
-    export default {
-        name: "relatorio",
-        components: {card, filterchart, chart, tablechart},
-        data: () => ({
-            filterLoad: false,
-            queryChart: {pergunta: 0, campus: 0, segmento: 0},
-            normalChart: false,
-            totalChart: false,
-            indicatorChart: {},
-            tableChart: [],
-            part_pessoas: 0
-        }),
-        methods: {
-            changeNormal(val) {
-                this.normalChart = val;
-            },
-            changeTotal(val) {
-                this.totalChart = val;
-            },
-            updateChart(val) {
-                this.queryChart = val
-            },
-            chartData(val) {
-                this.indicatorChart = val.indicador;
-                this.tableChart = val.data;
-                this.part_pessoas = val.total
-            },
-        },
-        mounted() {
-        }
-    }
+export default {
+  name: "relatorio",
+  components: {card, filterchart, chart, tablechart},
+  data: () => ({
+    filterLoad: false,
+    queryChart: {pergunta: 0, campus: 0, segmento: 0},
+    normalChart: false,
+    totalChart: false,
+    indicatorChart: {},
+    tableChart: [],
+    part_pessoas: 0
+  }),
+  methods: {
+    changeNormal(val) {
+      this.normalChart = val;
+    },
+    changeTotal(val) {
+      this.totalChart = val;
+    },
+    updateChart(val) {
+      this.queryChart = val
+    },
+    chartData(val) {
+      this.indicatorChart = val.indicador;
+      this.tableChart = val.data;
+      this.part_pessoas = val.total
+    },
+  },
+  mounted() {
+  }
+}
 </script>
 
 <style scoped>
-  #chart-indicator {
-    text-align: center;
-    line-height: 3em;
-    border-radius: 5px;
-    color: #fff;
-  }
+#chart-indicator {
+  text-align: center;
+  line-height: 3em;
+  border-radius: 5px;
+  color: #fff;
+}
 </style>
