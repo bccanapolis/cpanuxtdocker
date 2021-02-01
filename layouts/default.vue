a3da3efbfe45dd2cd1cedf306a0462198fe14da5
Bruno de Araujo Alves <devbaraus@gmail.com>
2021-02-01 18:22:22 -0300

    Update Flash Message - Alteração do flash message, position: absolute;

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="wrapper">
-    <FlashMessage style="z-index: 9999;" position="right bottom"></FlashMessage>
     <div class="sidebar" data-color="green" data-image="/img/sidebar-4.jpg">
       <div class="sidebar-wrapper">
         <div class="logo">
@@ -20,7 +19,8 @@
         </div>
 
         <navbar></navbar>
-        <a style="width: 100%; text-align: center; position: absolute; bottom: 5px; padding: 10px; margin-top: 30px;" href="http://computacaoifg.com.br"><img
+        <a style="width: 100%; text-align: center; position: absolute; bottom: 5px; padding: 10px; margin-top: 30px;"
+           href="http://computacaoifg.com.br"><img
           width="200" src="/img/ciencia-computacao-pequeno.png"
           alt="Logo Curso de Ciência da Computação - IFG Campus Anápolis"></a>
       </div>
@@ -68,71 +68,72 @@
       </div>
 
 
-<!--      <footer class="footer">-->
-<!--        <div class="container-fluid">-->
-<!--          &lt;!&ndash;          <nav class="pull-left">&ndash;&gt;-->
-<!--          &lt;!&ndash;            <ul>&ndash;&gt;-->
-<!--          &lt;!&ndash;              <li>&ndash;&gt;-->
-<!--          &lt;!&ndash;                <a href="#">&ndash;&gt;-->
-<!--          &lt;!&ndash;                  Home&ndash;&gt;-->
-<!--          &lt;!&ndash;                </a>&ndash;&gt;-->
-<!--          &lt;!&ndash;              </li>&ndash;&gt;-->
+      <!--      <footer class="footer">-->
+      <!--        <div class="container-fluid">-->
+      <!--          &lt;!&ndash;          <nav class="pull-left">&ndash;&gt;-->
+      <!--          &lt;!&ndash;            <ul>&ndash;&gt;-->
+      <!--          &lt;!&ndash;              <li>&ndash;&gt;-->
+      <!--          &lt;!&ndash;                <a href="#">&ndash;&gt;-->
+      <!--          &lt;!&ndash;                  Home&ndash;&gt;-->
+      <!--          &lt;!&ndash;                </a>&ndash;&gt;-->
+      <!--          &lt;!&ndash;              </li>&ndash;&gt;-->
 
-<!--          &lt;!&ndash;            </ul>&ndash;&gt;-->
-<!--          &lt;!&ndash;          </nav>&ndash;&gt;-->
-<!--          <p class="copyright pull-right">-->
-<!--          </p>-->
-<!--        </div>-->
-<!--      </footer>-->
+      <!--          &lt;!&ndash;            </ul>&ndash;&gt;-->
+      <!--          &lt;!&ndash;          </nav>&ndash;&gt;-->
+      <!--          <p class="copyright pull-right">-->
+      <!--          </p>-->
+      <!--        </div>-->
+      <!--      </footer>-->
 
     </div>
+    <FlashMessage style="z-index: 9999; position: absolute;" position="right bottom"/>
 
   </div>
 
 </template>
 
 <script>
-  import {EventBus} from "../eventbus";
-  import navbar from "../components/navbar";
+import {EventBus} from "../eventbus";
+import navbar from "../components/navbar";
 
-  export default {
-    components: {
-      navbar
-    },
-    created() {
-      EventBus.$on('flash-error', msg => {
-        this.flashMessage.error({
-          title: msg.title,
-          message: msg.text,
-          time: 5000
-        });
-      })
-      EventBus.$on('flash-warning', msg => {
-        this.flashMessage.warning({
-          title: msg.title,
-          message: msg.text,
-          time: 5000
-        });
-      })
-      EventBus.$on('flash-info', msg => {
-        this.flashMessage.info({
-          title: msg.title,
-          message: msg.text,
-          time: 5000
-        });
-      })
-      EventBus.$on('flash-success', msg => {
-        this.flashMessage.success({
-          title: msg.title,
-          message: msg.text,
-          time: 5000
-        });
-      })
-    },
-    mounted() {
+export default {
+  components: {
+    navbar
+  },
+  created() {
+    EventBus.$on('flash-error', msg => {
+      this.flashMessage.error({
+        title: msg.title,
+        message: msg.text,
+        time: 5000
+      });
+    })
+    EventBus.$on('flash-warning', msg => {
+      this.flashMessage.warning({
+        title: msg.title,
+        message: msg.text,
+        time: 5000
+      });
+    })
+    EventBus.$on('flash-info', msg => {
+      this.flashMessage.info({
+        title: msg.title,
+        message: msg.text,
+        time: 5000
+      });
+    })
+    EventBus.$on('flash-success', msg => {
+      this.flashMessage.success({
+        title: msg.title,
+        message: msg.text,
+        time: 5000
+      });
+    })
+  },
+  mounted() {
 
-    }
   }
+}
 </script>
 
 <style>
