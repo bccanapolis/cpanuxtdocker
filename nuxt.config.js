ecf097981ef7de8130e67dfe83e5b37da7250c1a
devbaraus <me@baraus.dev>
2021-04-08 13:31:49 -0300

    Next Gen

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,17 +1,28 @@
 const manifest = {
-  name: 'IFG Comissão Própria de Avaliação',
-  short_name: 'IFG CPA',
-  description: 'Plataforma de questionários e relatórios da Comissão Própria de Avaliação do Instituto Federal de Goiás.',
-  lang: 'pt',
-  display: 'standalone',
-  background_color: '#ffffff',
-  theme_color: '#79b334',
-  site_url: 'http://cpa.bcc.anapolis.ifg.edu.br',
-  keywords: ['questionarios', 'relatorio', 'ifg', 'perguntas', 'repostas', 'cpa', 'comissão', 'própria', 'avaliação']
-}
+  name: "IFG Comissão Própria de Avaliação",
+  short_name: "IFG CPA",
+  description:
+    "Plataforma de questionários e relatórios da Comissão Própria de Avaliação do Instituto Federal de Goiás.",
+  lang: "pt",
+  display: "standalone",
+  background_color: "#ffffff",
+  theme_color: "#79b334",
+  site_url: "http://cpa.bcc.anapolis.ifg.edu.br",
+  keywords: [
+    "questionarios",
+    "relatorio",
+    "ifg",
+    "perguntas",
+    "repostas",
+    "cpa",
+    "comissão",
+    "própria",
+    "avaliação",
+  ],
+};
 
 export default {
-  target: 'static',
+  target: "static",
   generate: {
     // routes: [
     //   '/questionario/s4UkHMQC',
@@ -19,109 +30,129 @@ export default {
     //   '/questionario/4jn7qduk',
     //   '/questionario/g3YTAfpT',
     // ],
-    exclude: [
-      /\^\/questionario/
-    ]
+    exclude: [/\^\/questionario/],
   },
   /*
-  ** Headers of the page
-  */
+   ** Headers of the page
+   */
   head: {
     htmlAttrs: {
-      lang: 'pt-br',
+      lang: "pt-br",
     },
-    title: manifest['name'],
+    title: manifest["name"],
     meta: [
-      {charset: 'utf-8'},
-      {name: 'viewport', content: 'width=device-width, initial-scale=1'},
-      {hid: 'description', name: 'description', content: manifest['description']},
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      {
+        hid: "description",
+        name: "description",
+        content: manifest["description"],
+      },
 
       // open graph / facebook
-      {property: 'og:type', content: 'website'},
-      {property: 'og:title', content: manifest['name']},
-      {property: 'og:site_name', content: manifest['name']},
-      {property: 'og:description', content: manifest['description']},
+      { property: "og:type", content: "website" },
+      { property: "og:title", content: manifest["name"] },
+      { property: "og:site_name", content: manifest["name"] },
+      { property: "og:description", content: manifest["description"] },
 
-      {property: 'og:image', content: `${manifest['site_url']}/img/metaimage_app.png`},
-      {property: 'og:image:type', content: 'image/png'},
-      {property: 'og:image:width', content: '1200'},
-      {property: 'og:image:height', content: '628'},
+      {
+        property: "og:image",
+        content: `${manifest["site_url"]}/img/metaimage_app.png`,
+      },
+      { property: "og:image:type", content: "image/png" },
+      { property: "og:image:width", content: "1200" },
+      { property: "og:image:height", content: "628" },
       //
-      {property: 'og:image', itemprop: 'image', content: `${manifest['site_url']}/img/metaimage_app_whatsapp.png`},
-      {property: 'og:image:type', content: 'image/png'},
-      {property: 'og:image:width', content: '400'},
-      {property: 'og:image:height', content: '400'},
+      {
+        property: "og:image",
+        itemprop: "image",
+        content: `${manifest["site_url"]}/img/metaimage_app_whatsapp.png`,
+      },
+      { property: "og:image:type", content: "image/png" },
+      { property: "og:image:width", content: "400" },
+      { property: "og:image:height", content: "400" },
 
       // twitter
-      {property: 'twitter:card', content: 'summary_large_image'},
-      {property: 'twitter:url', content: manifest['site_url']},
-      {property: 'twitter:title', content: manifest['name']},
-      {property: 'twitter:description', content: manifest['description']},
-      {property: 'twitter:image', content: `${manifest['site_url']}/img/metaimage_app.png`}
+      { property: "twitter:card", content: "summary_large_image" },
+      { property: "twitter:url", content: manifest["site_url"] },
+      { property: "twitter:title", content: manifest["name"] },
+      { property: "twitter:description", content: manifest["description"] },
+      {
+        property: "twitter:image",
+        content: `${manifest["site_url"]}/img/metaimage_app.png`,
+      },
     ],
     link: [
-      {rel: 'icon', type: 'image/x-icon', href: '/favicon.ico'},
-      {rel: 'stylesheet', href: '/css/bootstrap.min.css'},
-      {rel: 'stylesheet', href: '/css/animate.min.css'},
-      {rel: 'stylesheet', href: '/css/light-bootstrap-dashboard.css?v=1.4.0'},
-      {rel: 'stylesheet', href: 'https://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css'},
-      {rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Roboto:400,700,300'},
-      {rel: 'stylesheet', href: '/css/pe-icon-7-stroke.css'},
+      { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
+      { rel: "stylesheet", href: "/css/bootstrap.min.css" },
+      { rel: "stylesheet", href: "/css/animate.min.css" },
+      { rel: "stylesheet", href: "/css/light-bootstrap-dashboard.css?v=1.4.0" },
+      {
+        rel: "stylesheet",
+        href:
+          "https://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css",
+      },
+      {
+        rel: "stylesheet",
+        href: "https://fonts.googleapis.com/css?family=Roboto:400,700,300",
+      },
+      { rel: "stylesheet", href: "/css/pe-icon-7-stroke.css" },
     ],
     script: [
-      {src: '/js/jquery.3.2.1.min.js', type: 'text/javascript'},
-      {src: '/js/bootstrap.min.js', type: 'text/javascript'},
-      {src: '/js/bootstrap-notify.js', type: 'text/javascript'},
-      {src: '/js/light-bootstrap-dashboard.js?v=1.4.0', type: 'text/javascript'},
-    ]
+      { src: "/js/jquery.3.2.1.min.js", type: "text/javascript" },
+      { src: "/js/bootstrap.min.js", type: "text/javascript" },
+      { src: "/js/bootstrap-notify.js", type: "text/javascript" },
+      {
+        src: "/js/light-bootstrap-dashboard.js?v=1.4.0",
+        type: "text/javascript",
+      },
+    ],
   },
   /*
-  ** Customize the progress-bar color
-  */
-  loading: {color: '#fff'},
+   ** Customize the progress-bar color
+   */
+  loading: { color: "#fff" },
   /*
-  ** Global CSS
-  */
+   ** Global CSS
+   */
   css: [],
   /*
-  ** Plugins to load before mounting the App
-  */
-  plugins: [
-    {src: '~plugins/apexcharts-plugin.js', mode: 'client'},
-    {src: '~/plugins/vue-flash-message.js', mode: 'client'}
-    // {src: '~plugins/vuelidate'}
-  ],
+   ** Plugins to load before mounting the App
+   */
+  plugins: [{ src: "~plugins/apexcharts-plugin.js", mode: "client" }],
   /*
-  ** Nuxt.js dev-modules
-  */
+   ** Nuxt.js dev-modules
+   */
   buildModules: [
-    ['@nuxtjs/google-analytics', {
-      id: 'UA-158720692-1'
-    }]
+    [
+      "@nuxtjs/google-analytics",
+      {
+        id: "UA-158720692-1",
+      },
+    ],
   ],
   /*
-  ** Nuxt.js modules
-  */
+   ** Nuxt.js modules
+   */
   modules: [
     // Doc: https://axios.nuxtjs.org/usage
-    '@nuxtjs/axios',
+    "@nuxtjs/axios",
   ],
   /*
-  ** Axios module configuration
-  ** See https://axios.nuxtjs.org/options
-  */
+   ** Axios module configuration
+   ** See https://axios.nuxtjs.org/options
+   */
   axios: {
     baseURL: process.env['API_HOST'] || 'https://api.cpa.bcc.anapolis.ifg.edu.br/v1'
-    // baseURL: 'http://127.0.0.1:8000/v1'
+    // baseURL: "http://127.0.0.1:8000/v1",
   },
   /*
-  ** Build configuration
-  */
+   ** Build configuration
+   */
   build: {
     /*
-    ** You can extend webpack config here
-    */
-    extend(config, ctx) {
-    }
-  }
-}
+     ** You can extend webpack config here
+     */
+    extend(config, ctx) {},
+  },
+};
